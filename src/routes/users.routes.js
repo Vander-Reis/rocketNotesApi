24bb49fc6cf56989cea0821735c8bad46d61388b
Vander-Reis <vanderreis2017@outlook.com>
2022-06-07 16:13:41 -0300

    adicionando tratamento de erros

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,22 +4,8 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-
-// Criação de um Middleware
-// function myMiddleware(request, response, next) {
-    
-//     if(!request.body.isAdmin) {
-//         return response.json({message: "User unauthorized"});
-//     }
-
-//     next()
-// }
-
-// Desta forma eu uso o middleware para todas as rotas
-// usersRoutes(myMiddleware);
-
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
