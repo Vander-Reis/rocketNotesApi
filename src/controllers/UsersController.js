ff02b7953707b884d43bd4c752968b0e98277836
Vander-Reis <vanderreis2017@outlook.com>
2022-06-09 15:33:10 -0300

    criptografia de senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,3 +1,5 @@
+const { hash } = require("bcryptjs");
+
 const AppError = require("../utils/AppError");
  
 const sqliteConection = require("../database/sqlite");
@@ -17,10 +19,14 @@ class UsersController {
         const database = await sqliteConection();
         const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
 
-
         if(checkUserExists) {
             throw new AppError("Este e-mail já está em uso.");
         }
+
+        const hashPassword = await hash(password, 8);
+
+        await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", 
+        [name, email, hashPassword]);
 
         return response.status(201).json();
 
