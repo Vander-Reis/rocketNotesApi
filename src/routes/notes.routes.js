c538ac76cd19817650ac36b37856580d2e482982
Vander-Reis <vanderreis2017@outlook.com>
2022-06-13 13:44:21 -0300

    created metod delete / index / show

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,6 +6,9 @@ const noutesRoutes = Router();
 
 const notesController = new NotesController();
 
+noutesRoutes.get("/", notesController.index);
 noutesRoutes.post("/:user_id", notesController.create);
+noutesRoutes.get("/:id", notesController.show);
+noutesRoutes.delete("/:id", notesController.delete);
 
 module.exports = noutesRoutes;
