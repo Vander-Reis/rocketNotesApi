d14810c510e3b65813c8ac37784945d53a98794d
Vander-Reis <vanderreis2017@outlook.com>
2022-07-18 15:54:20 -0300

    create route user authenticated

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,13 +1,16 @@
 const { Router } = require("express");
 
 const NotesController = require("../controllers/NotesController");
+const ensureAuthenticated = require("../middleware/ensureAuthenticated");
 
 const noutesRoutes = Router();
 
 const notesController = new NotesController();
 
+noutesRoutes.use(ensureAuthenticated);
+
 noutesRoutes.get("/", notesController.index);
-noutesRoutes.post("/:user_id", notesController.create);
+noutesRoutes.post("/", notesController.create);
 noutesRoutes.get("/:id", notesController.show);
 noutesRoutes.delete("/:id", notesController.delete);
 
